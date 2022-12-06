d348ca3eececce1d4ce3bfabd75ff185a63d452e
HospitalBus <simony1124@gmail.com>
2022-12-06 22:42:24 +0900

    add deploy stage

--- lib/mutating-pipeline.ts
+++ lib/mutating-pipeline.ts
@@ -2,11 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import { App, SecretValue, Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
 import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
-import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
-import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { Artifact, ArtifactPath, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, CodeDeployEcsDeployAction, EcsDeployAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
 import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { NetworkLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { EcsApplication, EcsDeploymentConfig, EcsDeploymentGroup } from 'aws-cdk-lib/aws-codedeploy';
+import { Cluster, Compatibility, ContainerImage, DeploymentControllerType, FargateService, FargateTaskDefinition, TaskDefinition } from 'aws-cdk-lib/aws-ecs';
 
 
 
@@ -56,9 +60,47 @@ class MyApplication extends Stage {
 class DeployPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
         const pipeline = new Pipeline(this, 'comment-pipeline', {
             pipelineName : 'comment-pipeline',
             crossAccountKeys : false, // save 1$/month
+        });
+
+        // Infra
+
+        const vpc = new Vpc(this, 'sample-comment-vpc', {
+            maxAzs : 2
+        });
+
+        const cluster = new Cluster(this, "sample-comment-cluster", {
+            vpc: vpc
+        });
+
+        const taskDefinition = new TaskDefinition(this, 'TaskDefinition', {
+            compatibility: Compatibility.FARGATE,
+            cpu: '256',
+            memoryMiB: '512',
+        });
+
+        const baseImage = 'public.ecr.aws/amazonlinux/amazonlinux:2022'
+        // the task will fail because the image would not have been built
+        const container = taskDefinition.addContainer('SampleCommentAppContainer', {
+            containerName: "sample-comment-container",
+            //image: ContainerImage.fromEcrRepository(this.my_repo),
+            image: ContainerImage.fromRegistry(baseImage),
+        });
+
+        container.addPortMappings({
+            containerPort:8080,
+            hostPort:8080,
+        })
+        
+        const service = new FargateService(this, 'comment-service', {
+            cluster,
+            taskDefinition,
+            deploymentController: {
+                type: DeploymentControllerType.CODE_DEPLOY,
+            },
         });
 
         /** 
@@ -176,31 +218,78 @@ class DeployPipelineStack extends Stack {
             stageName : 'Build',
             actions : [buildAction]
         })
+
+        // Deploy stage
+        // create ecs service and set its deployment controller to codedeploy
+
+        // create codedeploy app
+        const application = new EcsApplication(this, 'CodeDeployApplication', {
+            applicationName: 'MyApplication', // optional property
+        });
+
+        /**
+         * Create Deployment TG
+         */
+
+
+
+        // specify appspec -> maybe built from code build
+        // deploy
+
+        // const deploymentGroup = EcsDeploymentGroup.fromEcsDeploymentGroupAttributes(pipeline, 'CommentBlueGreenDG', {
+        //     application : application,
+        //     deploymentGroupName: 'comment-deploy-group',
+        //     // blueGreenDeploymentConfig: {
+        //     //     blueTargetGroup,
+        //     //     greenTargetGroup,
+        //     //     listener,
+        //     // },
+        //     deploymentConfig: EcsDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+        // })
+        const deployAction = new EcsDeployAction({
+            actionName: 'deployAction',
+            service: service,
+            imageFile: new ArtifactPath(buildOutput, `imagedefinitions.json`)
+        });
+
+        pipeline.addStage({
+            stageName : 'Deploy',
+            actions: [deployAction]
+        });
+
     }
 }
 
-// // a vpc is a logically isolated portion of aws cloud within a region
-// // create your own vpc within aws account
-// const vpc = new Vpc(this, 'comment-vpc', {
-//     // define CIDR block
-//     // vpc subnets will have a longer subnet subnet masking (16) than the CIDB block
-//     maxAzs : 2
-//     // ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-//     // natGateways : 0,
-//     // availabilityZones : ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"],
-//     // subnetConfiguration: [
-//     //     {
-//     //         name: 'private-comment',
-//     //         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-//     //         cidrMask: 24,
-//     //     },
-//     // ]
-// });
+// vpc, ecs, nlb, apigateway
+class InfraStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-// const nlb = new NetworkLoadBalancer(this, 'comment-nlb', {
-//     vpc,
-//     internetFacing: true
-// })
+        // a vpc is a logically isolated portion of aws cloud within a region
+        // create your own vpc within aws account
+        const vpc = new Vpc(this, 'comment-vpc', {
+            // define CIDR block
+            // vpc subnets will have a longer subnet subnet masking (16) than the CIDB block
+            maxAzs : 2
+            // ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+            // natGateways : 0,
+            // availabilityZones : ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"],
+            // subnetConfiguration: [
+            //     {
+            //         name: 'private-comment',
+            //         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+            //         cidrMask: 24,
+            //     },
+            // ]
+        });
+
+        const nlb = new NetworkLoadBalancer(this, 'comment-nlb', {
+            vpc,
+            internetFacing: true
+        })
+    }
+}
+
 
 // const listener = nlb.addListener('comment-nlb-listener', {
 //     port : 80,
